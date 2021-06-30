04426fd71de59e21a741723ad509dd5f8a6032b3
Velo <hartley.max@outlook.com>
2021-06-30 12:01:33 +0100

    redirect from home to results page, populate page with search query results

---
+++ client/results.js
@@ -0,0 +1,43 @@
+const url = 'http://localhost:5000/search?search=pie';
+
+function createUrlQuery() {
+	const query = window.location.href.split('???')[1];
+	const url = `http://localhost:5000/search?search=${query}`;
+	return url;
+}
+
+async function fetchData() {
+	const url = createUrlQuery();
+	const fetchResults = await fetch(url)
+		.then((res) => res.json())
+		.catch(console.error);
+	console.log(fetchResults);
+	return fetchResults;
+}
+
+function createResult({ link, snippet, title }) {
+	const div = document.createElement('div');
+	const linkA = document.createElement('a');
+	linkA.href = link;
+	linkA.textContent = link;
+	const titleH1 = document.createElement('h1');
+	titleH1.textContent = title;
+	const snippetP = document.createElement('p');
+	snippetP.textContent = snippet;
+
+	div.appendChild(linkA);
+	div.appendChild(titleH1);
+	div.appendChild(snippetP);
+	return div;
+}
+
+async function appendResultsToMain() {
+	const main = document.querySelector('main');
+	const searchResults = await fetchData();
+	searchResults.results.forEach((result) => {
+		const divToAppend = createResult(result);
+		main.appendChild(divToAppend);
+	});
+}
+
+appendResultsToMain();
